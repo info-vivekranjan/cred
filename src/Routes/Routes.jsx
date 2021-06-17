ae26c12c060d17828593876b181651bb24b14f59
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-06-17 08:15:22 +0530

    Merging blogs left files

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -23,6 +23,36 @@ import FixedDepositCalculator from '../Pages/calculators/FixedDepositCalculator'
 import CompoundInterestCalculator from '../Pages/calculators/CompoundInterestCalculator';
 import HomePage from '../Pages/HomePage';
 
+
+
+
+import { BlogHome } from './BlogHome';
+
+import { CreditCards } from './BlogCategory/CreditCards'
+import { CreditCardsPayment } from './BlogCategory/CreditCardsPayment';
+import { MoneyMatters } from './BlogCategory/MoneyMatters';
+import { CreditCardsDataPage } from './BlogCategory/CreditCardsDataPage'
+import { MoneyMattersDataPage } from './BlogCategory/MoneyMattersDataPage';
+import { CreditCardsPaymentDataPage } from './BlogCategory/CreditCardsPaymentDataPage';
+import { BookMarks } from './Bookmarks/Bookmarks';
+import { BestCreditCards } from './BlogCategory/BestCreditCards';
+import { BestCreditCardsDataPage } from './BlogCategory/BestCreditCardsDataPage';
+import { SocialAnnouncement } from './BlogCategory/SocialAnnouncement';
+import { SocialDataPage } from './BlogCategory/SocialAnnouncementDataPage';
+import { NavTabs } from './PostBlogCategory/AddAllBlogs';
+
+import { Chat } from "./Chat/Chat";
+import { Join } from "./Join/Join";
+import { RegisterForChat } from './RegisterForChat/RegisterForChat';
+import { ChatRequestAdminPage } from './ChatRequestAdmin/ChatRequestAdminPage';
+
+import { Tech } from './BlogCategory/Tech';
+import { TechDataPage } from './BlogCategory/TechDataPage';
+
+
+
+
+
 const Routes = () => {
     return (
         <Switch>
@@ -57,6 +87,87 @@ const Routes = () => {
             <Route path="/calculators/CompoundInterestCalculator" exact>
                 <CompoundInterestCalculator/>
             </Route>
+{/* ---------------------------------------- BLOGS ---------------------------------- */}
+
+
+
+                <Route path="/blogs" exact>
+                    <BlogHome />
+                </Route>
+                <Route path="/category/credit-cards" exact>
+                    <CreditCards />
+                </Route>
+                <Route path="/category/credit-card-payment" exact>
+                    <CreditCardsPayment />
+                </Route>
+                <Route path="/category/money-matters" exact>
+                    <MoneyMatters />
+                </Route>
+
+                <Route path="/category/best-credit-cards" exact>
+                    <BestCreditCards />
+                </Route>
+
+                <Route path="/category/social-announcement" exact>
+                    <SocialAnnouncement />
+                </Route>
+
+                <Route path="/category/tech" exact>
+                    <Tech />
+                </Route>
+
+
+                <Route path="/category/bookmarks" exact>
+                    <BookMarks />
+                </Route>
+
+                <Route path="/category/postblogs" exact>
+                    <NavTabs />
+                </Route>
+
+
+
+                <Route path="/register-for-meeting" exact>
+                    <RegisterForChat />
+                </Route>
+                <Route path="/joinmeeting" exact>
+                    <Join />
+                </Route>
+                <Route path="/chat">
+                    <Chat />
+                </Route>
+
+                <Route path="/admin/chatrequests">
+                    <ChatRequestAdminPage />
+                </Route>
+
+                <Route path="/category/credit-cards/:id" exact>
+                    <CreditCardsDataPage />
+                </Route>
+
+                <Route path="/category/money-matters/:id" exact>
+                    <MoneyMattersDataPage />
+                </Route>
+
+
+                <Route path="/category/credit-card-payment/:id" exact>
+                    <CreditCardsPaymentDataPage />
+                </Route>
+
+                <Route path="/category/best-credit-cards/:id" exact>
+                    <BestCreditCardsDataPage />
+                </Route>
+
+
+                <Route path="/category/social-announcement/:id" exact>
+                    <SocialDataPage />
+                </Route>
+
+                <Route path="/category/tech/:id" exact>
+                    <TechDataPage />
+                </Route>
+
+
 
             {/* ---------------------------------------- CAREERS ---------------------------------- */}
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './Context/AuthContext';
+import { BlogContextProvider } from './Context/BlogContextProvider';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
+<BlogContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+</BlogContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
